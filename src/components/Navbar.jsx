f1580701eed85055b64ddb16bacc499408b04e0d
WasifAli22 <wasifaliwasif22@gmail.com>
2023-05-04 08:10:15 +0500

    basic structure defined

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Stack from '@mui/joy/Stack';
+import { Link } from 'react-router-dom';
+ const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+export default Navbar
